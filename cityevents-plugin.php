4cfc212a6761e1e7a0a8c1b74001dc0a16862d2c
carlo <carlo.centrone@gerp.it>
2025-09-25 12:51:28 +0200

    piccole corr

--- cityevents-plugin.php
+++ cityevents-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: cityevents
  * Description: CityEvents – Pubblica l'agenda eventi culturali della tua citta'
- * Version: 0.1.7
+ * Version: 0.1.8
  * Author: Cognita.it
  * License: GPLv2 or later
  * Text Domain: cityevents
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) { exit; }
 class CityEvents_Plugin {
     const TD = 'cityevents';
 
-    const VERSION = '0.1.7';
+    const VERSION = '0.1.8';
 
     public static $url;
 
